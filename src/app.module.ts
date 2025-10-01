d3dbd131981a92e08657a339d29dc356ff973eb5
clementruch <144322316+clementruch@users.noreply.github.com>
2025-10-01 17:34:06 +0200

    debut

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST', 'localhost'),
+        port: config.get<number>('DB_PORT', 3310),
+        username: config.get<string>('DB_USER', 'root'),
+        password: config.get<string>('DB_PASS', 'root'),
+        database: config.get<string>('DB_NAME', 'lampe_aube'),
+        autoLoadEntities: true,
+        synchronize: true, // ⚠️ uniquement en dev
+      }),
+    }),
+  ],
 })
 export class AppModule {}
